2d4fe6974c5077a9b43e76ad6a353f7718a978cc
Lee Steiner <leealansteiner@gmail.com>
2018-05-29 23:14:21 -0400

    EoD5.29

--- UPE Excel Formatter/RowObject.cs
+++ UPE Excel Formatter/RowObject.cs
@@ -7,8 +7,13 @@ using System.Threading.Tasks;
 
 namespace UPE_Excel_Formatter
 {
+    /// <summary>
+    /// Contains a row number and list of cell objects (int Column, int Row, string Value)
+    /// </summary>
+
     public class RowObject :IEnumerable
     {
+
         public int RowNumber { get; set; }
         private List<CellObject> cells = new List<CellObject>();
         public List<CellObject> Cells
